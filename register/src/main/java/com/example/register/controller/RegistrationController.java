9b615cf1c5491853ee8c0dbe06b59a1bd249c605
LuongChu2004 <luongfgd@gmail.com>
2025-05-15 14:51:00 +0700

    register

---
+++ register/src/main/java/com/example/register/controller/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.example.register.controller;
+
+
+import com.example.register.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("user") User user) {
+        return "result";
+    }
+}
